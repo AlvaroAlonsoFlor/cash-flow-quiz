3ae11b40d4b7df2108db34300148f80ab5810cab
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-11-13 23:24:02 +0000

    Add /quiz route linking to the welcome screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import WelcomeScreen from './containers/WelcomeScreen';
 
 class App extends Component {
   render() {
     return (
-      <h1>App working</h1>
+      <Router>
+        <Route exact path='/quiz' component={WelcomeScreen}/>
+
+      </Router>
     );
   }
 }
---
+++ client/src/containers/WelcomeScreen.js
@@ -0,0 +1,15 @@
+import React, {Component, Fragment} from 'react';
+
+export default class WelcomeScreen extends Component {
+
+
+    render() {
+        return(
+            <Fragment>
+                <h2>Welcome to the Cash Flow Quiz!</h2>
+                <h4>What is your name?</h4>
+                <h4>Here goes a form component</h4>
+            </Fragment>
+        )
+    }
+}
