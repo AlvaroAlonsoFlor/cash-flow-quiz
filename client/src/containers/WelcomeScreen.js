fbe5fc5aa945f5521cecfeb0485970d7bca3557b
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-11-14 17:00:16 +0000

    Handle click in WelcomeScreen

--- client/src/containers/WelcomeScreen.js
+++ client/src/containers/WelcomeScreen.js
@@ -1,7 +1,17 @@
 import React, {Component, Fragment} from 'react';
 import NameForm from '../components/NameForm';
+import { CHANGE_POSITION } from '../constants/action-types';
 
 export default class WelcomeScreen extends Component {
+
+    handleClick() {
+       
+        window.store.dispatch({type: CHANGE_POSITION})
+        const position = window.store.getState().questionPosition  
+        
+        window.location.replace(`/questions/${position}`);
+        
+    }
 
 
     render() {
@@ -9,8 +19,11 @@ export default class WelcomeScreen extends Component {
             <Fragment>
                 <h2>Welcome to the Cash Flow Quiz!</h2>
                 <h4>What is your name?</h4>
+                
                 <NameForm />
-                <button>Next</button>
+                <button onClick={this.handleClick}>Next</button>
+                
+                
             </Fragment>
         )
     }
