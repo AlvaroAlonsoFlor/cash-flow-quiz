6c94a370166ffbf51141b2efffab8e523c20e5cd
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-11-15 19:40:07 +0000

    Remove questionPosition from index.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import store from './store/index';
-import {moveQuestionPosition} from "./actions/index.js"
 
 // Initialize store
 window.store = store
